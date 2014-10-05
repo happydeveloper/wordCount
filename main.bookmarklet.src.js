edbcbd259ca479b0b5921c1f2277e533d70f4399
egoing <egoing@gmail.com>
2014-10-06 00:40:27 +0900

    bookmarklet 기능 추가

--- main.bookmarklet.src.js
+++ main.bookmarklet.src.js
@@ -10,13 +10,10 @@ for(var name in wordSortTable){
     continue;
    wordSort.push([name, wordSortTable[name]]);
 }
-wordSort.sort(function(a, b) {return a[1] - b[1]});
-wordSort = wordSort.slice(-200);
-str = '';
+wordSort.sort(function(a, b) {return b[1] - a[1]});
+wordSort = wordSort.slice(0, 40);
+str = 'WordCounter.js (by 서툰 영어의 시대)\n';
 for(var i = 0; i < wordSort.length; i++){
-    str += wordSort[i][1] +"\t"+ wordSort[i][0] + "\n";
+    str += wordSort[i][1] +", "+ wordSort[i][0] + "\n";
 }
-console.log(str);
-console.log("%cWordCounter.js", "text-shadow: -0.06em 0 red, 0.06em 0 cyan; font-size:50px");
-console.log('by 서툰 영어의 시대 (http://opentutorials.org/module/1058)');
-
+alert(str);
